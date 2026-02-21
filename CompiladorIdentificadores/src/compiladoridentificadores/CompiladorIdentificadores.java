dd188949ec8bda118e10ddc2be8477687d607aaf
JulioCarrilloOrtizITZ <L23450186@zacatecas.tecnm.mx>
2026-02-20 19:43:21 -0600

    Runnable para mejor compilacion

--- CompiladorIdentificadores/src/compiladoridentificadores/CompiladorIdentificadores.java
+++ CompiladorIdentificadores/src/compiladoridentificadores/CompiladorIdentificadores.java
@@ -14,7 +14,10 @@ public class CompiladorIdentificadores {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
-    }
-    
+    java.awt.EventQueue.invokeLater(new Runnable() {
+        public void run() {
+            new Vista().setVisible(true);
+        }
+    });
+}
 }
