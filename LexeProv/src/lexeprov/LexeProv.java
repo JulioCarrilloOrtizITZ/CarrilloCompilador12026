424c800254adee6ca1adc8c042061574ad60c100
JulioCarrilloOrtizITZ <L23450186@zacatecas.tecnm.mx>
2026-03-16 13:32:36 -0600

    Arraylist creado

--- LexeProv/src/lexeprov/LexeProv.java
+++ LexeProv/src/lexeprov/LexeProv.java
@@ -4,14 +4,32 @@
  */
 package lexeprov;
 
+import java.util.ArrayList;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 /**
  *
  * @author julyc
  */
 public class LexeProv {
     public static void main(String[] args) {
+        String EjemploLex ="var x @ y = 42; if x >= 10 then write(x) for i down to 5 do begin end";
         String regex="[([a-zA-Z]\\w*)([1-9]\\d*)0|(==|!=|<=|>=)|([-+*/=<>;,.()@])\"]";
-        
+        Pattern pattern=Pattern.compile(regex);
+        Pattern matcher = pattern.compile(EjemploLex);
+           ArrayList<String> lex= new ArrayList<>();
+           while (matcher.find()) {
+            lex.add(matcher.group(0));
+        }
+          System.out.println("Tokens encontrados (" + lex.size() + "):");
+        System.out.println(lex);
+       
+        // Bonus: también lo mostramos bonito
+        System.out.println("\nLista ordenada:");
+        for (String t : lex) {
+            System.out.println(t);
+        }
     }
-    
 }
+
